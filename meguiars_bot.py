c5ee27a86666fcb95aca2ba404aa693cd08544f3
AvramT <131826332+AvramT@users.noreply.github.com>
2023-06-09 11:46:28 +1200

    Integrated menu into main app
    Created function for menu for user to choose either click & collect or delivery

--- meguiars_bot.py
+++ meguiars_bot.py
@@ -14,9 +14,36 @@ def welcome():
     print("*** My name is",name,"***")
     print("*** I'll be here to help you order New Zealand's finest car-care products. ***")
 
+# Menu for click & collect or delivery
+
+def order_type():
+    print("Is your order for click & collect or delivery?")
+
+    print("For click & collect, please enter 1.")
+    print("For delivery, please enter 2.")
+
+    while True:
+        try: 
+            delivery = int(input("Please enter a number. "))
+            if delivery >= 1 and delivery <= 2:
+                if delivery == 1:
+                    print("Click & Collect")
+                    break
+
+                elif delivery == 2:
+                    print("Delivery")
+                    break
+            else:
+                print("The number must be 1 or 2.")
+        except ValueError:
+            print("That is not a valid number.")
+            print("Please enter 1 or 2.")
+
+
 # Main function
 def main():
     welcome()
+    order_type()
 
 main()
 
