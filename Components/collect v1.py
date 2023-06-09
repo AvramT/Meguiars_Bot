5338f8ba20238497a8c13f52609d422789b2d073
AvramT <131826332+AvramT@users.noreply.github.com>
2023-06-09 12:15:54 +1200

    Created click & collect menu v1
    Basic instructions and input for name and phone number

---
+++ Components/collect v1.py
@@ -0,0 +1,16 @@
+# Bugs:
+# - Accepts blank input
+
+print("Please enter the pickup information.")
+
+# Customer name 
+name = input("Please enter your name. ")
+
+# Customer phone number
+phone = input("Please enter your phone number. ")
+
+print(name)
+print(phone)
+
+print("You will receive a text message from us once the item/s are ready for pickup.")
+
